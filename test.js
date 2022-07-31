68881ea6749eb5f660b53307bf2468f168d1a07f
FROSTYLAN <castillo089frosty@gmail.com>
2022-07-30 20:10:46 -0500

    fronted developer - pseudoclases enabled

--- test.js
+++ test.js
@@ -1,7 +1,4 @@
-document.addEventListener("DOMContentLoaded", function () {
-  document.getElementById("activar").onchange = toggleActivar;
-});
-function toggleActivar() {
-  var getInputText = document.querySelectorAll('.disabled input[type="text"]');
-  getInputText[0].disabled = !getInputText[0].disabled;
-}
+document.querySelector("button").onclick = function () {
+  window.print();
+};
+s;
