042b8a6e10171eaa69e08cfe0a356e53e7bbd049
FROSTYLAN <castillo089frosty@gmail.com>
2022-08-22 17:06:49 -0500

    Funciones en JavaScript

---
+++ 03-JavascriptBásico/03-funciones.js
@@ -0,0 +1,16 @@
+// Declarativas
+
+function miFuncion() {
+  return 3;
+}
+
+miFuncion();
+
+/***************************/
+// De Expresión (funciones anónimas)
+
+var suma = function (a, b) {
+  return a + b;
+};
+
+suma();
