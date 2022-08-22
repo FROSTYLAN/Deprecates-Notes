cebb74a46654f4068489bb824a1877bbe3a13df7
FROSTYLAN <castillo089frosty@gmail.com>
2022-08-22 15:08:46 -0500

    Variables en Javascript

---
+++ 03-JavascriptBásico/02-variables.js
@@ -0,0 +1,20 @@
+// Variable: Es un espacio reservado en memoria.
+
+var nombre = "Charles";
+
+// Declarar una variable.
+var edad; // -> undefined
+
+// Inicializar una variable.
+edad = 30;
+
+edad;
+
+var elementos = ["Computadora", "Celular"]; // -> String
+
+var persona = {
+  nombre: Charles,
+  edad: 30,
+};
+
+persona;
