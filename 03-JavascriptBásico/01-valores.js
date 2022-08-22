1e44a0ab25a916784e1fd2067ef9e44a4e646a10
FROSTYLAN <castillo089frosty@gmail.com>
2022-08-22 13:56:53 -0500

    Valores en javascript

---
+++ 03-JavascriptBásico/01-valores.js
@@ -0,0 +1,20 @@
+/* VALORES TIPO PRIMITIVO */
+
+40; // -> Numeros
+
+("Charles Castillo"); // -> Cadenas de texto (String)
+
+true; // -> Booleanos (Boolean) / verdadero / 1
+false; // -> Booleanos (Boolean) / falso / 0
+
+null; // -> Valores placeholder / espacio reservado / empty values
+undefined; // -> Valores placeholder / error / empty values
+
+/***************************/
+/* VALORES TIPO OBJETO */
+
+[1, 2, 3, 4, 5, 6, 7, 8]; // -> Arreglo (Arrays)
+
+{
+  nombre: "Charles"; // -> Valor tipo Objeto (Object)
+}
