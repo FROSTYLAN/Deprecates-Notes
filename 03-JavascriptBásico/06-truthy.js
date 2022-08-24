c6c107b23bf81063bd9bcd889259ac1a63b30376
FROSTYLAN <castillo089frosty@gmail.com>
2022-08-24 16:36:29 -0500

    Valores truthy

---
+++ 03-JavascriptBásico/06-truthy.js
@@ -0,0 +1,32 @@
+if (true) {
+}
+
+if ({}) {
+}
+
+if ([]) {
+}
+
+if (42) {
+}
+
+if ("foo") {
+}
+
+if (new Date()) {
+}
+
+if (-42) {
+}
+
+if (3.14) {
+}
+
+if (-3.14) {
+}
+
+if (Infinity) {
+}
+
+if (-Infinity) {
+}
