09203ddcd3b7a1487501aef730af804fecc11d8a
FROSTYLAN <castillo089frosty@gmail.com>
2022-08-24 16:56:48 -0500

    Op. de comparación

---
+++ 03-JavascriptBásico/09-op_comparacion.js
@@ -0,0 +1,23 @@
+// Igual que
+x == y;
+
+// Estrictamente igual que
+x === y;
+
+// Mayor que
+x > y;
+
+// Mayor igual que
+x >= y;
+
+// Menor que
+x < y;
+
+// Menor igual que
+x <= y;
+
+// Diferente que
+x != y;
+
+// Estrictamente diferente que
+x !== y;
