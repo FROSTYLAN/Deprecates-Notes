37fc45416c73a568ebb7eb95896ee7a395a21d0a
FROSTYLAN <castillo089frosty@gmail.com>
2022-08-24 16:53:53 -0500

    Op. de asignación

---
+++ 03-JavascriptBásico/08-op_asignacion.js
@@ -0,0 +1,20 @@
+// Asignación
+x = y;
+
+// Asignación de adición
+x += y;
+
+// Asignación de resta
+x -= y;
+
+// Asignación de multiplicación
+x *= y;
+
+// Asignación de división
+x /= y;
+
+//Asignación de residuo
+x %= y;
+
+// Asignación de exponenciación
+x **= y;
