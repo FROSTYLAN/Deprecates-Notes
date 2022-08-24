973e48f561454aef859e0df96d7caf6f15f2f4fe
FROSTYLAN <castillo089frosty@gmail.com>
2022-08-24 18:18:15 -0500

    if else

---
+++ 03-JavascriptBásico/12-if.js
@@ -0,0 +1,24 @@
+// if
+if (true) {
+  console.log("Hola");
+}
+if (false) {
+  console.log("Soy falso");
+}
+
+// if else
+if (true) {
+  console.log("Soy verdadero");
+} else {
+  console.log("Soy falso");
+}
+
+// else if
+var edad = 18;
+if (edad === 18) {
+  console.log("Puedes votar, será tu primera vez");
+} else if (edad < 18) {
+  console.log("Puedes votar de nuevo");
+} else {
+  console.log("Aun no tienes edad para votar");
+}
