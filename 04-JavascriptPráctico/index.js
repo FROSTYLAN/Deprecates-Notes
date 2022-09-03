f89815c988f879c27f0878a1affc4b5cb81ffd46
FROSTYLAN <castillo089frosty@gmail.com>
2022-09-03 13:51:05 -0500

    addEventListener - submit!

--- 04-JavascriptPráctico/index.js
+++ 04-JavascriptPráctico/index.js
@@ -1,12 +1,14 @@
 const h1 = document.querySelector("h1");
 const input1 = document.querySelector("#calculo1");
 const input2 = document.querySelector("#calculo2");
+const form = document.querySelector("#form");
 const btn = document.querySelector("#btnCalculo");
 const pResult = document.querySelector("#result");
 
-btn.addEventListener("click", btnOnclick);
+form.addEventListener("submit", btnOnclick);
 
-function btnOnclick() {
+function btnOnclick(event) {
+  event.preventDefault();
   const sumaInputs = input1.value + input2.value;
   pResult.innerText = "Resultado: " + sumaInputs;
 }
