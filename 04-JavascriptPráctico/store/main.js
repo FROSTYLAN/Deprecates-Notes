380588226ebf8bc17ddf3c43a8c97f103a6e3d14
FROSTYLAN <castillo089frosty@gmail.com>
2022-09-06 13:04:23 -0500

    Lista de productos: HTML a partir de JavaScritpt

--- 04-JavascriptPráctico/store/main.js
+++ 04-JavascriptPráctico/store/main.js
@@ -4,6 +4,7 @@ const burgerMenuIcon = document.querySelector(".menu");
 const carMenuIcon = document.querySelector(".navbar-shopping-cart");
 const mobileMenu = document.querySelector(".mobile-menu");
 const aside = document.querySelector(".product-detail");
+const cardsContainer = document.querySelector(".cards-container");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
 burgerMenuIcon.addEventListener("click", toggleMobileMenu);
@@ -36,3 +37,44 @@ function toggleCarMenuAside() {
 
   aside.classList.toggle("inactive");
 }
+
+function renderProduct(arr) {
+  for (product of arr) {
+    const productCard = document.createElement("div");
+    productCard.classList.add("product-card");
+
+    const img = document.createElement("img");
+    img.setAttribute("src", product.image);
+
+    const productInfo = document.createElement("div");
+    productInfo.classList.add("product-info");
+
+    const divInfo = document.createElement("div");
+    const productPrice = document.createElement("p");
+    productPrice.innerText = "$" + product.price;
+    const productName = document.createElement("p");
+    productName.innerText = product.name;
+
+    const figureInfo = document.createElement("figure");
+    const iconCart = document.createElement("img");
+    iconCart.setAttribute("src", "../assets/icons/bt_add_to_cart.svg");
+
+    figureInfo.appendChild(iconCart);
+    divInfo.append(productPrice, productName);
+    productInfo.append(divInfo, figureInfo);
+    productCard.append(img, productInfo);
+    cardsContainer.appendChild(productCard);
+  }
+}
+
+const productList = [];
+for (let i = 0; i < 20; i++) {
+  productList.push({
+    name: "Bike",
+    price: "120",
+    image:
+      "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  });
+}
+
+renderProduct(productList);
