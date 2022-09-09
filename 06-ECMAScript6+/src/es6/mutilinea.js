b2e7eff7a0d8b76bc6bc7f28469080bb9b069bb9
FROSTYLAN <castillo089frosty@gmail.com>
2022-09-09 12:18:27 -0500

    multilínea

---
+++ 06-ECMAScript6+/src/es6/mutilinea.js
@@ -0,0 +1,15 @@
+let lorem =
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit.\n" +
+  "Veniam veritatis cumque ipsa itaque repellendus officiis,\n" +
+  "aspernatur molestiae dolore eum debitis!";
+
+// es6
+
+let lorem2 = `
+Lorem ipsum dolor sit amet consectetur adipisicing elit.
+Veniam veritatis cumque ipsa itaque repellendus officiis,
+aspernatur molestiae dolore eum debitis!
+`;
+
+console.log(lorem);
+console.log(lorem2);
