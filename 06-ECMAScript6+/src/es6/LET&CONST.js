75cb03db21a2069040b09edd041f88a1e8cfd776
FROSTYLAN <castillo089frosty@gmail.com>
2022-09-09 12:34:54 -0500

    Let & Const

---
+++ 06-ECMAScript6+/src/es6/LET&CONST.js
@@ -0,0 +1,13 @@
+{
+  var globalVar = "Global Var";
+}
+
+{
+  let globalLet = "Global Let";
+  console.log(globalLet);
+}
+
+console.log(globalVar);
+
+const a = "b";
+// a = "a"; TypeError: Assignment to constant variable.
