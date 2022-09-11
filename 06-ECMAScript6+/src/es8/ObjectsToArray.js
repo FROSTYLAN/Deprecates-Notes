15901667e710c26e6d83f72f8a09fb03b97b94d2
FROSTYLAN <castillo089frosty@gmail.com>
2022-09-11 14:59:48 -0500

    Object To Values

---
+++ 06-ECMAScript6+/src/es8/ObjectsToArray.js
@@ -0,0 +1,14 @@
+const data = {
+  frontend: "React",
+  backend: "NodeJS",
+  design: "Figma",
+};
+
+const entries = Object.entries(data);
+console.log(entries);
+
+const keys = Object.keys(data);
+console.log(keys);
+
+const values = Object.values(data);
+console.log(values);
