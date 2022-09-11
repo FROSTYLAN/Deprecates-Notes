75711efa2e5f9e019f5309d2eadca5ee790148f4
FROSTYLAN <castillo089frosty@gmail.com>
2022-09-11 14:20:46 -0500

    Includes - es7

---
+++ 06-ECMAScript6+/src/es7/Includes.js
@@ -0,0 +1,13 @@
+let numbers = [1, 2, 3, 7, 8];
+
+if (numbers.includes(7)) {
+  console.log("Si se encuentra el valor 7.");
+} else {
+  console.log("No se encuentra");
+}
+
+if (numbers.includes(9)) {
+  console.log("Si se encuentra el valor 9.");
+} else {
+  console.log("No se encuentra");
+}
