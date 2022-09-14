ff265d5cdb2bc5fb8cfc561438cb9c90e568a80a
FROSTYLAN <castillo089frosty@gmail.com>
2022-09-14 12:03:21 -0500

    Eliminar espacios en blanco

---
+++ 06-ECMAScript6+/src/es10/trim.js
@@ -0,0 +1,7 @@
+let hello = "             Hello world!";
+console.log(hello);
+console.log(hello.trimStart());
+
+let hello2 = "Hello world!            ";
+console.log(hello2);
+console.log(hello2.trimEnd());
