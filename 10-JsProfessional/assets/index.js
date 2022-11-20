d8a7a7712c876bb41f2be4b74028840697e759d5
FROSTYLAN <castillo089frosty@gmail.com>
2022-11-20 00:25:54 -0500

    Getters y setters

--- 10-JsProfessional/assets/index.js
+++ 10-JsProfessional/assets/index.js
@@ -1,11 +1,11 @@
-import MediaPlayer from './MediaPlayer.js';
-import AutoPlay from './plugins/AutoPlay.js'
+import MediaPlayer from "./MediaPlayer.js";
+import AutoPlay from "./plugins/AutoPlay.js";
 
 const video = document.querySelector("video");
 const btnTogglePlay = document.querySelector(".btnTogglePlay");
-const btnToggleMute = document.querySelector(".btnToggleMute")
+const btnToggleMute = document.querySelector(".btnToggleMute");
 
-const player = new MediaPlayer({ el: video, plugins: [new AutoPlay()]});
+const player = new MediaPlayer({ el: video, plugins: [new AutoPlay()] });
 
 btnTogglePlay.onclick = () => player.togglePlay();
 btnToggleMute.onclick = () => player.toggleMute();
