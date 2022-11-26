5170fb115e1bd3f5843bc926ffb4b6f968e9dc4c
FROSTYLAN <castillo089frosty@gmail.com>
2022-11-26 15:48:42 -0500

    Tipos básicos en JavaScript

--- 10-JsProfessional/ejercicios/typescript/index.ts
+++ 10-JsProfessional/ejercicios/typescript/index.ts
@@ -7,3 +7,48 @@ function add(a: number, b: number)  {
 let sum = add(2,5);
 
 console.log(sum);
+
+
+// TIPOS BÁSICOS
+
+// boolean
+let muted: boolean = true;
+muted = false;
+
+// números
+let age = 6;
+let numerador: number = 42;
+let denominador: number = age;
+let resultado = numerador / denominador;
+
+// string
+let nombre: string = 'Charles';
+let saludo = `Me llamo ${nombre}.`
+
+// arreglos
+
+let people: string[] = [];
+people = ['Isabel', 'Nicole', 'Charles'];
+// people.push(9000);
+
+let peopleAndNumbers: Array<string|number> = [];
+peopleAndNumbers.push('Miguel Ignacio de las Casas');
+peopleAndNumbers.push(8);
+
+// Enum
+enum Color {
+    Rojo = 'Rojo',
+    Verde = 'Verde',
+    Azul = 'Azul',
+    Amarillo = 'Amarillo',
+}
+
+let favoriteColor: Color = Color.Azul;
+console.log(`Mi color favorito es ${favoriteColor}`);
+
+// Any
+let comodin: any = 'Joker';
+comodin = {type: 'Wildcard'};
+
+// Object 
+let someObject: object = {type: 'Wildcard'};
