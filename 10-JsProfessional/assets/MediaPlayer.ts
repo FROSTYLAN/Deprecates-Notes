21b10f9533bba4e2c7b1a31151f53cf2c97920a1
FROSTYLAN <castillo089frosty@gmail.com>
2022-12-02 16:19:42 -0500

    Convertir el proyecto a TypeScript

--- 10-JsProfessional/assets/MediaPlayer.ts
+++ 10-JsProfessional/assets/MediaPlayer.ts
@@ -1,43 +1,41 @@
 class MediaPlayer {
+  media: HTMLMediaElement;
+  plugins: Array<any>;
+
   constructor(config) {
     this.media = config.el;
     this.plugins = config.plugins || [];
 
-    this._initPlugins();
+    this.initPlugins();
   }
-  _initPlugins() {
-    const player = {
-      play: () => this.play(),
-      pause: () => this.pause(),
-      media: this.media,
-      get muted() {
-        return this.media.muted;
-      },
-      set muted(value) {
-        this.media.muted = value;
-      },
-    };
 
+  private initPlugins() {
     this.plugins.forEach((plugin) => {
-      plugin.run(player);
+      plugin.run(this);
     });
   }
+
   mute() {
     this.media.muted = true;
   }
+
   unmute() {
     this.media.muted = false;
   }
+
   toggleMute() {
     if (this.media.muted) this.unmute();
     else this.mute();
   }
+
   play() {
     this.media.play();
   }
+
   pause() {
     this.media.pause();
   }
+  
   togglePlay() {
     if (this.media.paused) this.play();
     else this.pause();
--- 10-JsProfessional/assets/index.ts
+++ 10-JsProfessional/assets/index.ts
@@ -1,10 +1,10 @@
-import MediaPlayer from "./MediaPlayer.js";
-import AutoPlay from "./plugins/AutoPlay.js";
-import AutoPause from "./plugins/AutoPause.ts";
+import MediaPlayer from "./MediaPlayer";
+import AutoPlay from "./plugins/AutoPlay";
+import AutoPause from "./plugins/AutoPause";
 
 const video = document.querySelector("video");
-const btnTogglePlay = document.querySelector(".btnTogglePlay");
-const btnToggleMute = document.querySelector(".btnToggleMute");
+const btnTogglePlay: any = document.querySelector(".btnTogglePlay");
+const btnToggleMute: any = document.querySelector(".btnToggleMute");
 
 const player = new MediaPlayer({
   el: video,
